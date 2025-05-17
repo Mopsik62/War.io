18ce96dbaceda2a832002ad02f11b9c285c8a04d
Mopsik62 <74981499+Mopsik62@users.noreply.github.com>
2025-05-17 12:27:02 +0300

    Base Shooting

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -1,19 +1,36 @@
 using UnityEngine;
 using War.io.Movement;
+using War.io.Shooting;
 
 namespace War.io
 {
-    [RequireComponent(typeof(CharacterMovementController))]
+    [RequireComponent(typeof(CharacterMovementController), typeof(ShootingController))]
     public class PlayerCharacter : MonoBehaviour
     {
-        private CharacterMovementController _characterMovementController;
+        [SerializeField]
+        private Weapon _baseWeaponPrefab;
+
+        [SerializeField]
+        private Transform _hand;
+
         private IMovementDirectionSource _movementDirectionSource;
+
+        private CharacterMovementController _characterMovementController;
+        private ShootingController _shootingController;
+
 
 
         protected void Awake()
         {
-            _characterMovementController = GetComponent<CharacterMovementController>();
             _movementDirectionSource = GetComponent<IMovementDirectionSource>();
+
+            _characterMovementController = GetComponent<CharacterMovementController>();
+            _shootingController = GetComponent<ShootingController>();
+        }
+
+        protected void Start()
+        {
+            _shootingController.SetWeapon(_baseWeaponPrefab, _hand);
         }
 
         // Update is called once per frame
